e3c0999ce715f3411ee3090ffb0ed75831d6bf7f
Raheem Orekoya <tundeorekoya1@gmail.com>
2023-12-30 05:27:22 +0100

    added my social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,13 +7,13 @@ const Footer = () => {
           <h4 className='text-2xl font-semibold text-zinc-700'>REDOXX</h4>
           <ul className='flex gap-4 items-center'>
                <li>
-                    <a href="#"><Instagram size={30} strokeWidth={1.5}/></a>
+                    <a href="https://www.instagram.com/redoxx.code/"><Instagram size={30} strokeWidth={1.5}/></a>
                </li>
                <li>
-                    <a href="#"><Twitter size={30} strokeWidth={1.5}/></a>
+                    <a href="https://twitter.com/Redoxx_Code"><Twitter size={30} strokeWidth={1.5}/></a>
                </li>
                <li>
-                    <a href="#"><Github size={30} strokeWidth={1.5}/></a>
+                    <a href="https://github.com/R4heem-Orekoya"><Github size={30} strokeWidth={1.5}/></a>
                </li>
           </ul>
      </footer>
